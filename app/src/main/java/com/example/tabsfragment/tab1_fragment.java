c0b5ebb5f2d01477b7f2169f5cf6b2f4d4301a8e
yasmeenmahmoud <yasmeenmahmoud@gmail.com>
2018-09-24 00:22:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tabsfragment/tab1_fragment.java
@@ -0,0 +1,26 @@
+package com.example.tabsfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yasmeen on 7/18/2018.
+ */
+
+public class tab1_fragment extends Fragment {
+    public tab1_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment1_layout, container, false);
+    }
+}
